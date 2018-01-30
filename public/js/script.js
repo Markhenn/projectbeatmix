06550b5a96ed6aa41ab35d91f6bd12e8e30f8a95
Mark <markhenningerwork@gmail.com>
2018-01-30 15:39:59 +0100

    add presetHandler function unfinished

--- public/js/script.js
+++ public/js/script.js
@@ -118,6 +118,39 @@ const clear = drumArray => {
     }
   };
 
+
+
+//  If the index was valid, presetHandler should also return a second element in the array. for 'GET' requests, that element should be the preset array at that array index. For 'PUT' requests, it should save the newPresetArray to that index and then also return it as the second element.
+
+//  If you are testing presets with the app itself, you will need to stop and re-start your server to see the changes you write in presetHandler.js take effect.
+
+const presetHandler = (request, presetsIndex, newPresetArray) => {
+
+  if (presetsIndex > 3 || presetsIndex < 0) {
+    return ['404'];
+  }
+
+//  if (request !== 'GET' || request !== 'PUT') {
+//    return ['400'];
+//  }
+
+  switch (request) {
+    case 'GET':
+      
+      return ['200', presetsIndex];
+      break;
+    case 'PUT':
+      return ['200', presetsIndex, newPresetArray];
+      break;
+    default:
+      return ['400'];
+      break;
+  }
+
+
+};
+
+/*
 toggleDrum('kicks', 15);
 toggleDrum('kicks', 2);
 toggleDrum('kicks', 15);
@@ -132,3 +165,5 @@ console.log(kicks);
 //console.log(kicks);
 //clear('kicks')
 //console.log(kicks);
+
+*/
